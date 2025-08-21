8acbec2301eb60d8d4b6f999ea32d1fa1b25b3df
dlworua <leegaegyum@gmail.com>
2025-08-21 17:39:18 +0900

    refactor: 상품 모델 코드 분리

---
+++ lib/models/product.dart
@@ -0,0 +1,13 @@
+class Product {
+  final String imageName;
+  final String name;
+  final String description;
+  final int price;
+
+  Product({
+    required this.imageName,
+    required this.name,
+    required this.description,
+    required this.price,
+  });
+}
