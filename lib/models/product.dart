bd5e3837a4fed6da108f97faf26adcfa7c0e1b8b
dlworua <leegaegyum@gmail.com>
2025-08-21 21:52:26 +0900

    feat: 상품 모델 image path속성추가

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,11 +1,12 @@
 class Product {
   final String imageName;
+  final String imagePath;
   final String name;
   final String description;
   final int price;
-
   Product({
     required this.imageName,
+    required this.imagePath,
     required this.name,
     required this.description,
     required this.price,
